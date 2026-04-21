e9d16fa8305aefae703d57329f00aa1cfda11507
Dpaslord <Davidpascuallorenzo@gmail.com>
2026-04-21 08:23:29 +0200

    Initial Commit

---
+++ PokedexApi/PokedexApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApiApp.swift
+//  PokedexApi
+//
+//  Created by David Pascual Lorenzo on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
